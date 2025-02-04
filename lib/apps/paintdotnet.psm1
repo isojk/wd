e628ad7755d3dfbb55be005694760f6e2153cbc3
isojk <isojk@isojk.net>
2025-02-04 13:40:09 +0100

    Add new application: Paint.NET

---
+++ lib/apps/paintdotnet.psm1
@@ -0,0 +1,43 @@
+$ErrorActionPreference = "Stop"
+
+Import-Module $PSScriptRoot\..\core.psm1 -DisableNameChecking -Scope Local
+Import-Module $PSScriptRoot\..\essentials.psm1 -DisableNameChecking -Scope Local
+
+$APP_ID = "paintdotnet"
+
+function hook {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)] [object] $InstallationHandlers,
+        [Parameter(Mandatory = $true)] [object] $ConfigurationHandlers
+    )
+
+    process {
+        $InstallationHandlers[$APP_ID] = {
+            [CmdletBinding()]
+            param (
+                [Parameter(Position = 0, Mandatory = $true)] [object] $profile
+            )
+
+            process {
+                wdChocoInstallPackage -Id "paint.net"
+            }
+        }
+
+        $ConfigurationHandlers[$APP_ID] = {
+            [CmdletBinding()]
+            param (
+                [Parameter(Position = 0, Mandatory = $true)] [object] $Profile,
+                [Parameter(Mandatory = $false)] [int] $Level = 1
+            )
+
+            process {
+                $data = (wdCoreGetDataDir)
+                $private = (wdCoreGetPrivateDataDir)
+                # Nothing to work on yet
+            }
+        }
+    }
+}
+
+Export-ModuleMember -Function hook
